fbb1a996513bec598fd233c43223122fba4a36b2
George701 <chernikov.georgii@gmail.com>
2019-07-26 13:25:16 +0300

    Initial commit & react - bootstrap form

---
+++ notetoself/src/components/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { Form, FormControl, Button } from 'react-bootstrap';
+
+class App extends Component {
+    constructor(){
+        super();
+
+        this.state = {
+            text: ''
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>Note to Self</h2>
+                <Form inline>
+                    <FormControl onChange={event => {this.setState({text: event.target.value})}}/>
+                    {' '}
+                    <Button variant="secondary" onClick={() => console.log(this.state)}>Submit</Button>
+                </Form>
+            </div>
+        )
+    }
+}
+
+export default App;
