ac4499c8c5276bcf86207285cfec84953b4c9365
George701 <chernikov.georgii@gmail.com>
2019-08-03 11:39:49 +0300

    Stateless functional components: Stack & Main

--- flashcardpro/src/components/Main.js
+++ flashcardpro/src/components/Main.js
@@ -1,19 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
 import StackList from './StackList';
 
-class Main extends Component {
-
-    render() {
-        return (
-            <div>
-                <StackList/>
-                <hr/>
-                <Link to='/stack_form'><h4>Create a New Stack</h4></Link>
-            </div>
-        )
-    }
+const Main = () => {
+    return (
+        <div>
+            <StackList/>
+            <hr/>
+            <Link to='/stack_form'><h4>Create a New Stack</h4></Link>
+        </div>
+    )
 }
 
 export default Main;
+
--- flashcardpro/src/components/Stack.js
+++ flashcardpro/src/components/Stack.js
@@ -1,36 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import Card from './Card'
 
-export class Stack extends Component {
-    render() {
-        // console.log('stack props: ', this.props);
-        const { title, cards } = this.props.stack;
-        // console.log(title, cards)
-        if(cards !== undefined && title !== undefined){
-            return (
-                <div>
-                    <Link to='/' className='link-home'>
-                        <h4>Home</h4>
-                    </Link>
-                    <h3>{title}</h3>
-                    <br />
-                    {
-                        cards.map(card => {
-                            return <Card key={card.id} card={card}/>
-                        })
-                    }
-                </div>
-            )
-        }else{
-            return <h2>Loading</h2>
-        }
+export const Stack = ({stack: {title, cards}}) => {
+    if(cards !== undefined && title !== undefined){
+        return (
+            <div>
+                <Link to='/' className='link-home'>
+                    <h4>Home</h4>
+                </Link>
+                <h3>{title}</h3>
+                <br />
+                {
+                    cards.map(card => {
+                        return <Card key={card.id} card={card}/>
+                    })
+                }
+            </div>
+        )
+    }else{
+        return <h2>Loading</h2>
     }
 }
 
 const mapStateToProps = state => {
-    // console.log("Stack: ", state);
     return { stack: state.stack}
 }
 
